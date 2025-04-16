cfbb08f2e46843c29a2543e3596df8f987c006d4
Cecil Wagner <wagnercecil@gmail.com>
2025-04-16 23:53:34 +0200

    Updated the core testbench and added a new program to test all the alu instructions The program results in a 1 in registers 5 to 31 (with some missing for load and store)

--- sim/tb_core.sv
+++ sim/tb_core.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-import instruction_utils::disassemble;
+import instruction_utils::*;
 
 module tb_core;
     
@@ -29,50 +29,59 @@ module tb_core;
         if ($test$plusargs("debug")) begin
             forever @(posedge clk) begin
                 if (!reset) begin
-                    $display("PC: 0x%08h | Instr: 0x%08h | %s", 
-                            dut.pc, dut.instr, disassemble(dut.instr));
-                    
-                    // Display source register values when relevant
-                    // if (dut.is_addi || dut.is_add || dut.is_beq || dut.is_bne) begin
-                    //     $display("    x%0d = 0x%08h (%0d), x%0d = 0x%08h (%0d)",
-                    //             dut.rs1, dut.rs1_data, dut.rs1_data,
-                    //             dut.rs2, dut.rs2_data, dut.rs2_data);
-                    // end
+                    $display("PC: 0x%08h | Instr: 0x%08h | %s \t| rs1=%0d | rs2=%0d",
+                            dut.pc, dut.instr, instruction_utils::disassemble(dut.instr), dut.rs1_data, dut.rs2_data);
                     if (dut.reg_file.write_en && dut.rd != 0) begin
                         $display("    Register Write: x%0d = %0d", dut.rd, dut.result);
                     end
                 end
             end
-        end else $display("set +debug to print debug information");        
+        end else $display("set +debug to print debug information");
     end
 
     // Test program
     initial begin
-        // Initialize instruction memory with test program
-        // Program calculates sum of numbers from 5 down to 1 (5+4+3+2+1 = 15)
-        // 0: addi x1, x0, 5      // x1 = 5 (counter)
-        // 4: addi x2, x0, 0      // x2 = 0 (sum)
-        // 8: loop:
-        // 8: add x2, x2, x1      // sum += counter
-        // c: addi x1, x1, -1     // counter--
-        //10: bne x1, x0, -8      // loop if counter != 0 (jump to 8)
-        //14: addi x0, x0, 0      // loop exit; nop
-        //18: beq x0, x0, -4      // infinit loop to not crash the simulation
+        if ($test$plusargs("sum_test")) begin
+            // Initialize instruction memory with test program
+            // Program calculates sum of numbers from 5 down to 1 (5+4+3+2+1 = 15)
+            // 0: addi x1, x0, 5      // x1 = 5 (counter)
+            // 4: addi x2, x0, 0      // x2 = 0 (sum)
+            // 8: loop:
+            // 8: add x2, x2, x1      // sum += counter
+            // c: addi x1, x1, -1     // counter--
+            //10: bne x1, x0, -8      // loop if counter != 0 (jump to 8)
+            //14: addi x0, x0, 0      // loop exit; nop
+            //18: beq x0, x0, -4      // infinit loop to not crash the simulation
 
-        // reading the instructions from a file, this behaves differntly in vivado and iverilog
-        // vivado fills the buffer from 0 upwards, and iverilog seems to fill from top to bottom
-        // therefore i can leave the memory oversized in vivado but not for iverilog
-        $readmemh("./sim/sum_test.txt", dut.i_mem.mem, 0);
+            // reading the instructions from a file, this behaves differntly in vivado and iverilog
+            // vivado fills the buffer from 0 upwards, and iverilog seems to fill from top to bottom
+            // therefore i can leave the memory oversized in vivado but not for iverilog
+            $readmemh("./sim/sum_test.txt", dut.i_mem.mem, 0);
 
-        // Wait for completion
-        #500;
-        
-        // Check final register values
-        assert(dut.reg_file.registers[1] == 0 && dut.reg_file.registers[2] == 15) begin
-            $display("TEST PASSED: x1 = 0, x2 = 15 as expected");
-        end else begin
-            $display("TEST FAILED: x1 = %0d (expected 0), x2 = %0d (expected 15)", dut.reg_file.registers[1], dut.reg_file.registers[2]);
+            // Wait for completion
+            #500;
+            
+            // Check final register values
+            if(dut.reg_file.registers[1] == 0 && dut.reg_file.registers[2] == 15) begin
+                $display("TEST PASSED: x1 = 0, x2 = 15 as expected");
+            end else begin
+                $display("TEST FAILED: x1 = %0d (expected 0), x2 = %0d (expected 15)", dut.reg_file.registers[1], dut.reg_file.registers[2]);
+            end
         end
+        if ($test$plusargs("alu_test")) begin
+            // this program should test every instruction(except for load/store as they are not implemented yet)
+            // each innstruction test should result in a 1 in the respective register (x5-x30)
+            $readmemh("./sim/alu_test.hex", dut.i_mem.mem, 0);
+            // Wait for completion
+            #800;
+            for (int i = 5; i < 31; i = i + 1) begin
+                if(dut.reg_file.registers[i] != 1) begin
+                     $display("Register x%0d failed!", i);
+                end
+            end
+            $display("TEST DONE");
+        end
+            
         $finish;
     end
 
@@ -91,3 +100,4 @@ module tb_core;
     end
 
 endmodule
+
--- src/instruction_memory.sv
+++ src/instruction_memory.sv
@@ -9,7 +9,7 @@ module instruction_memory(
 );
     
     // Memory should be byte addressable but for now i'll leave it as is and just divide the addr/4
-    reg [31:0] mem [0:6];
+    reg [31:0] mem [0:54];
     
     // for now i just divide the memory by
     assign data = reset ? 32'b0 : mem[addr/4];
